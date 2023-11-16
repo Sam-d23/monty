b13a4570f6600adf365604ac570b501867979de1
Sam-d23 <samkiragu05@gmail.com>
2023-11-15 20:55:54 -0800

    Task 0

--- push.c
+++ push.c
@@ -1,37 +1,49 @@
 #include "monty.h"
-/**
- * f_push - add node to stack
- * @head: stack head
- * @counter: line_number
- * Return: none
-*/
-void f_push(stack_t **head, unsigned int counter)
-{
-	int n, i = 0, flag = 0;
+#include <ctype.h>
 
-	if (bus.arg)
+/**
+ * check_for_digit - checks whether a string only contains digits
+ * @arg: string to check
+ * Return: 0 if digits only, else 1
+ */
+static int check_for_digit(char *arg)
+{
+	int j;
+
+	for (j = 0; arg[j]; j++)
 	{
-		if (bus.arg[0] == '-')
-			i++;
-		for (; bus.arg[i] != '\0'; i++)
-		{
-			if (bus.arg[i] > 57 || bus.arg[i] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE); }}
-	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE); }
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
-		addnode(head, n);
-	else
-		addqueue(head, n);
+		if (arg[j] == '-' && j == 0)
+			continue;
+		if (isdigit(arg[j]) == 0)
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * m_push - push an integer onto the stack
+ * @stack: double pointer to the beginning of the stack
+ * @line_number: script line number
+ * Return: none
+ */
+void f_push(stack_t **stack, unsigned int line_number)
+{
+	char *arg;
+	int n;
+
+	arg = strtok(NULL, "\n\t\r ");
+	if (arg == NULL || check_for_digit(arg))
+	{
+		dprintf(STDOUT_FILENO,
+			"L%u: usage: push integer\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(arg);
+	if (!add_node(stack, n))
+	{
+		dprintf(STDOUT_FILENO, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	var.stack_len++;
 }
