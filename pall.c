a3602ae32a089ab7104647df371f28e0704077e6
Sam-d23 <samkiragu05@gmail.com>
2023-11-16 00:38:07 -0800

    Task 0

--- pall.c
+++ pall.c
@@ -1,18 +1,17 @@
 #include "monty.h"
-/**
- * f_pall - function that prints the stack
- * @head: stack head
- * @counter: no used
- * Return: none
-*/
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
 
-	h = *head;
-	if (h == NULL)
-		return;
+/**
+ * pall - prints all the stack values
+ * @stack: double pointer to the head of the stack
+ * @line_number: number of a line of the file
+ * Return: none
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *h = *stack;
+
+	(void) line_number;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
--- push.c
+++ push.c
@@ -1,49 +1,33 @@
 #include "monty.h"
-#include <ctype.h>
 
 /**
- * check_for_digit - checks whether a string only contains digits
- * @arg: string to check
- * Return: 0 if digits only, else 1
- */
-static int check_for_digit(char *arg)
-{
-	int j;
-
-	for (j = 0; arg[j]; j++)
-	{
-		if (arg[j] == '-' && j == 0)
-			continue;
-		if (isdigit(arg[j]) == 0)
-			return (1);
-	}
-	return (0);
-}
-
-/**
- * m_push - push an integer onto the stack
- * @stack: double pointer to the beginning of the stack
- * @line_number: script line number
+ * push - pushes new node to the stack
+ * @stack: double pointer to the stack's head
+ * @line_number: number of a line of the file
  * Return: none
  */
-void f_push(stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, unsigned int line_number)
 {
-	char *arg;
-	int n;
+	stack_t *node;
+	char *num;
 
-	arg = strtok(NULL, "\n\t\r ");
-	if (arg == NULL || check_for_digit(arg))
+	num = strtok(NULL, DELIMS);
+	if (num == NULL)
 	{
-		dprintf(STDOUT_FILENO,
-			"L%u: usage: push integer\n",
-			line_number);
+		printf("L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	n = atoi(arg);
-	if (!add_node(stack, n))
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
 	{
-		dprintf(STDOUT_FILENO, "Error: malloc failed\n");
+		printf("Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	var.stack_len++;
+	node->n = atoi(num);
+	node->prev = NULL;
+	node->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = node;
+	*stack = node;
 }
