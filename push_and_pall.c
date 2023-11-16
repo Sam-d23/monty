ef32c078cc48661a5862123a3a27e2f0e0fd7015
Sam-d23 <samkiragu05@gmail.com>
2023-11-15 22:19:56 -0800

    task 0.new

--- push_and_pall.c
+++ push_and_pall.c
@@ -1,22 +1,33 @@
 #include "monty.h"
 
-void push(Stack *stack, int value) 
+/**
+ * push - runs opcode push
+ * @stack: pointer to stack
+ * @value: value to be pushed
+ * Return: none
+ */
+void push(Stack *stack, int value)
 {
-    if (stack->top == STACK_SIZE - 1) 
-    {
-        fprintf(stderr, "Error: Stack overflow\n");
-        exit(EXIT_FAILURE);
-    }
+	if (stack->top == STACK_SIZE - 1)
+	{
+		fprintf(stderr, "Error: Stack overflow\n");
+		exit(EXIT_FAILURE);
+	}
 
-    stack->stack[++stack->top] = value;
+	stack->stack[++stack->top] = value;
 }
 
-void pall(Stack *stack) 
+/**
+ * pall - runs opcode pall
+ * @stack: pointer to stack
+ * Return: none
+ */
+void pall(Stack *stack)
 {
-    int i;
+	int i;
 
-    for (i = stack->top; i >= 0; i--) 
-    {
-        printf("%d\n", stack->stack[i]);
-    }
+	for (i = stack->top; i >= 0; i--)
+	{
+		printf("%d\n", stack->stack[i]);
+	}
 }
