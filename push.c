2948077fddb59d411e2095bab0d990c3852e14be
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-07-05 17:35:55 +0300

    0

--- push.c
+++ push.c
@@ -1,54 +1,37 @@
 #include "monty.h"
 /**
- * _push_opcode - function to execute the push opcode.
- * @stackPtr: pointer to top of the stack.
- * @line_num: line number in the file.
- * Return: void
- */
-void _push_opcode(stack_t **stackPtr, unsigned int line_num)
+ * f_push - Adds node to the stack
+ * @head: The stack's head
+ * @counter: The line_number
+ * Return: None
+*/
+void f_push(stack_t **head, unsigned int counter)
 {
-	stack_t *new_node = NULL;
-	int mod_int = 0, check = 0;
+	int n, j = 0, flag = 0;
 
-	line_num += 0;
-	check = _isdigit(line_num, stackPtr);
-	if (check == 0)
-		_geterrmsg(2, line_num, stackPtr);
-
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-		_geterrmsg(2, line_num, stackPtr);
-	new_node->prev = NULL;
-	new_node->next = NULL;
-	mod_int = atoi(c->oc_arg);
-	new_node->n = mod_int;
-
-	if (*stackPtr != NULL)
+	if (bus.arg)
 	{
-		(*stackPtr)->prev = new_node;
-		new_node->next = *stackPtr;
-		*stackPtr = new_node;
-	}
-	*stackPtr = new_node;
-}
-
-/**
- * _isdigit - checks for a digit.
- * @line_num: line number in the file.
- * @stackPtr: pointer to top of the file.
- * Return: 1 if digit is read, 0 otherwise.
- */
-int _isdigit(unsigned int line_num, stack_t **stackPtr)
-{
-	int j = 0;
-
-	if (c->oc_arg == NULL)
-		_geterrmsg(2, line_num, stackPtr);
-	while (c->oc_arg[j] != '\0')
-	{
-		if ((c->oc_arg[j] < 48 || c->oc_arg[j] > 57) && c->oc_arg[j] != 45)
-			return (0);
-		j++;
-	}
-	return (1);
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
